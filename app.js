6a176106116f8ec52592ae9fb301f1baa488014e
s4ijem-san <mejiaseverywhere@gmail.com>
2026-04-25 14:26:19 -0400

    inicio del código JS , se puede agregar productos al carrito

---
+++ app.js
@@ -0,0 +1,34 @@
+let carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
+
+mostrarCarrito();
+
+function agregarCarrito(nombre,precio) {
+
+    let producto = carrito.find(p => p.nombre === nombre);
+
+    if (producto) {
+        producto.cantidad++;
+    } else {
+        carrito.push({ nombre, precio , cantidad: 1})
+    }
+
+    sessionStorage.setItem("carrito", JSON.stringify (carrito));
+    mostrarCarrito();
+}
+
+function mostrarCarrito() {
+    let lista = document.getElementById("listaCarrito");
+    let total = 0;
+
+    lista.innerHTML="";
+
+    carrito.forEach(p => {
+        let li = document.createElement("li");
+        li.textContent = `${p.nombre} - $${p.precio} x ${p.cantidad}`;
+        lista.appendChild(li);
+
+        total += p.precio * p.cantidad;
+    });
+
+    document.getElementById("total").textContent = "Total: $" + total;
+}
